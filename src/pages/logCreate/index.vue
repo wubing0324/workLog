f2a315ebec4554852770a0fc3f80483845d5d5b5
Bing Wu <bingwu@BingdeMacBook-Pro.local>
2020-02-07 16:34:54 +0800

    保存页面完成

--- src/pages/logCreate/index.vue
+++ src/pages/logCreate/index.vue
@@ -1,17 +1,40 @@
 <template>
   <div class="logCreate">
-    <van-cell title="选择单个日期" :value="formatDate" title-style="text-align: left" @click="show = true" is-link />
-    <van-calendar v-model="show" :show-confirm="false" @confirm="onConfirm" />
+    <van-cell title="日期" :value="formatDate" title-style="text-align: left;" @click="show = true" is-link />
+    <van-calendar v-model="show" :default-date="date" :show-confirm="false" @confirm="onConfirm" :min-date="minDate" :max-date="maxDate" />
+
+    <van-field
+      v-model="message"
+      rows="2"
+      autosize
+      type="textarea"
+      maxlength="50"
+      placeholder="请输入工作内容"
+      show-word-limit
+    />
+    <van-field
+      class="floatStyle"
+      v-model="hours"
+      label="小时"
+      :formatter="formatter"
+      placeholder="投入时间"
+    />
+    <van-button class="btn" type="info" block @click="save">保存</van-button>
   </div>
 </template>
 
 <script>
+import day from 'dayjs'
 export default {
   name: 'logCreate',
   data () {
     return {
       date: '',
+      hours: '',
       show: false,
+      minDate: '',
+      maxDate: '',
+      message: '',
       week: {
         0: '周日',
         1: '周一',
@@ -33,22 +56,65 @@ export default {
     }
   },
   created () {
+    this.minDate = new Date(day().subtract(1, 'year'))
+    this.maxDate = new Date(day().add(1, 'year'))
     this.date = new Date()
   },
   methods: {
     onConfirm (date) {
       this.show = false
       this.date = date
+    },
+    formatter (value) {
+      // 过滤输入的数字
+      return value.replace(/[^0-9]/g, '')
+    },
+    save () {
+      const { date, hours, message } = this
+      let params = {
+        date,
+        hours,
+        message
+      }
+      this.$router.push({ name: 'logDetail', params })
     }
   }
 }
 </script>
 
 <style lang="less">
-.titleStyle{
+.van-cell{
+  margin: 10px auto;
+}
+.titleStyle, .van-cell__title{
   text-align: left;
 }
 .van-icon-arrow::before {
     margin-top: 4px;
+}
+.floatStyle{
+  display: block;
+  .van-cell__title{
+    float: right;
+    text-align: right;
+  }
+  .van-cell__value{
+    float: left;
+  }
+}
+.btn{
+  background: #2288EE;
+  border-radius: 3px;
+  width: 335px;
+  height: 44px;
+  margin: 20px auto;
+  font-family: PingFangSC-Regular;
+  font-size: 16px;
+  color: #FFFFFF;
+  letter-spacing: 0;
+  line-height: 16px;
+  &:hover{
+    background: #1F7CD9;
+  }
 }
 </style>
--- src/pages/logDetail/index.vue
+++ src/pages/logDetail/index.vue
@@ -1,14 +1,47 @@
 <template>
   <div class="logDetail">
-    <van-button type="default">woworkListrk</van-button>
+    <van-button type="default" @click="changeDay('prev')">前一天</van-button>
+    <van-button type="default" @click="goLogList">{{ formatDate }}</van-button>
+    <van-button type="default" @click="changeDay('next')">后一天</van-button>
   </div>
 </template>
 
 <script>
+import day from 'dayjs'
+
 export default {
   name: 'logDetail',
   data () {
     return {
+      day: day,
+      date: day(),
+      week: {
+        0: '周日',
+        1: '周一',
+        2: '周二',
+        3: '周三',
+        4: '周四',
+        5: '周五',
+        6: '周六'
+      }
+    }
+  },
+  computed: {
+    formatDate () {
+      return `${this.date.format('YYYY年MM月DD')} ${this.week[this.date.format('d')]}`
+    }
+  },
+  methods: {
+    changeDay (type) {
+      if (type === 'prev') {
+        this.date = day(this.date).subtract(1, 'day')
+        console.log(this.date)
+      } else {
+        this.date = day(this.date).add(1, 'day')
+      }
+    },
+    goLogList () {
+      this.$router.push({ name: 'logList', params: { defaultDate: this.date } })
     }
   }
 }
