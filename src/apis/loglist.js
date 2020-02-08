b43141f87cdec48d33c89e95f7dbaea81a443440
xuuu <xujunxiong@Xuuu.local>
2020-02-08 16:47:39 +0800

    queryUserWorkLogSum接口完成

--- src/apis/loglist.js
+++ src/apis/loglist.js
@@ -2,7 +2,7 @@ import request from '../utils/request'
 
 export function queryUserWorkLogSum (data) {
   return request({
-    url: '/sys/userworklog/queryUserWorkLogSum?',
+    url: '/sys/userworklog/queryUserWorkLogSum',
     method: 'post',
     data
   })
